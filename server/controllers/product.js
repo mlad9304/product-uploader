fc22b52396662eaa3f85df0f1f65244c9c10377a
rndev80 <rndev80@gmail.com>
2019-07-05 00:02:35 +0300

    Implemented product list and post

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -5,12 +5,25 @@ exports.list = (req, res) => {
   const query = req.query || {};
 
   Product.apiQuery(query)
-    .select('_id variable1 variable2 variable3 variable4')
+    .select('_id productName variable1 variable2 variable3 variable4')
     .then(products => {
       res.json(products);
     })
     .catch(err => {
       logger.error(err);
       res.status(422).send(err.errors);
+    });
+};
+
+exports.post = (req, res) => {
+  const data = Object.assign({}, req.body) || {};
+
+  Product.create(data)
+    .then(product => {
+      res.json(product);
+    })
+    .catch(err => {
+      logger.error(err);
+      res.status(500).send(err);
     });
 };
